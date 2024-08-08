4706ba03f5eee3d27d2c4218ba9de9e85dcbe31f
2115425Amar <amar2115425@akgec.ac.in>
2024-08-08 07:40:24 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import Categories from './pages/Categories';
 import CategoryProduct from './pages/CategoryProduct';
 import CartPage from './pages/CartPage';
 import Chatbot from './pages/Chatbot';
+import { useEffect } from 'react';
 // import Search from './pages/search';
 //  import Search from './pages/Search.js';
 
@@ -74,3 +75,4 @@ function App() {
 }
 
 export default App;
+
